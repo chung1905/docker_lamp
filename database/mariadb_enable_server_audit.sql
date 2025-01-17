ea04117f6e2cbef3dc075eff4e7d0a1082505698
chung <me@chungn.com>
2025-01-18 00:34:22 +0700

    server_audit.so

--- database/mariadb_enable_server_audit.sql
+++ database/mariadb_enable_server_audit.sql
@@ -1,4 +1,4 @@
 INSTALL PLUGIN server_audit SONAME 'server_audit.so';
-SET GLOBAL server_audit_logging=ON;
-SET GLOBAL server_audit_events='QUERY_DML_NO_SELECT';
+SET GLOBAL server_audit_logging = ON;
+SET GLOBAL server_audit_events = 'QUERY_DML_NO_SELECT';
 SHOW GLOBAL VARIABLES LIKE 'server_audit%';
